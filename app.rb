38e89e482004104ad6ce45f152bd771836cbf5b6
Amee Covarrubias <ameecova@yahoo.com>
2019-06-06 23:53:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 class Application < Sinatra::Base
   # Write your code here!
+  
+  get '/' do 
+    erb :index
+  end 
+  
+  get '/greet' do 
+    @user = User.new 
+    @user.name = params[:user_name]
+    erb :greet
+  end 
 
 end
